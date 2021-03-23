f6096cd74247f904ae864672ed54246531e6e785
Marcin Kowalczyk <kowalczyk.m@int.pl>
2021-03-23 17:24:17 +0100

    todo 4_2

--- src/main/java/pl/kowalczyk/codility/Zad_4_1.java
+++ src/main/java/pl/kowalczyk/codility/Zad_4_1.java
@@ -1,21 +1,25 @@
 package pl.kowalczyk.codility;
 
+import java.util.*;
+
 public class Zad_4_1 {
 
     public static void main(String[] args) {
-
+        int[] A = {1,3,1,4,2,3,5,4};
+        System.out.println(solution(5, A));
     }
 
-    // A - tablica
-    // N - liczba wartości
-    // P - wartość
-    public int solution(int[] A) {
+    public static int solution(int X, int[] A) {
+        Set<Integer> availableFields = new HashSet<>();
 
-        int sumL;
-        int sumR;
+        for (int i = 0; i < A.length; i++) {
+            availableFields.add(A[i]);
+            if (availableFields.size() == X){
+                return i;
+            }
+        }
 
-
-        return 0;
+        return -1;
     }
 
 }
