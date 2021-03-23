a4e8a08b0e9f36dcde33df2f2ad88f8d57b61f04
Marcin Kowalczyk <kowalczyk.m@int.pl>
2021-03-23 14:06:42 +0100

    todo: Zad 4

--- src/main/java/pl/kowalczyk/codility/Zad_3_3.java
+++ src/main/java/pl/kowalczyk/codility/Zad_3_3.java
@@ -1,21 +1,34 @@
 package pl.kowalczyk.codility;
 
 public class Zad_3_3 {
+    // A - tablica
+    // N - ilość wartości
+    // P - wartość
 
     public static void main(String[] args) {
-
+        int[] A = {-1000, 1000};
+        System.out.println(solution(A));
     }
 
-    // A - tablica
-    // N - liczba wartości
-    // P - wartość
-    public int solution(int[] A) {
 
-        int sumL;
-        int sumR;
+    public static int solution(int[] A) {
+        int minDiff = Integer.MAX_VALUE;
 
+        int sumL = 0;
+        int sumR = 0;
 
-        return 0;
+        int totalSum = 0;
+
+        for (int value : A) {
+            totalSum += value;
+        }
+        for (int i = 0; i < A.length - 1; i++) {
+            sumL += A[i];
+            sumR = totalSum - sumL;
+            minDiff = Math.min(minDiff, Math.abs(sumL - sumR));
+
+        }
+        return minDiff;
     }
 
 }
