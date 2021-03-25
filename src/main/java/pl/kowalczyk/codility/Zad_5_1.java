b1edaa06ecde4b0207a5a9fa2b2a64d5689d516c
Marcin Kowalczyk <kowalczyk.m@int.pl>
2021-03-25 14:07:51 +0100

    corrected task 5_1

--- src/main/java/pl/kowalczyk/codility/Zad_5_1.java
+++ src/main/java/pl/kowalczyk/codility/Zad_5_1.java
@@ -3,26 +3,51 @@ package pl.kowalczyk.codility;
 public class Zad_5_1 {
 
     public static void main(String[] args) {
-        int A = 6, B = 11, K = 2;
+        int A = 0, B = 0, K = 11;
         System.out.println(solution(A, B, K));
     }
 
     public static int solution(int A, int B, int K) {
+
+        if (B < K) {
+            if (A != 0) {
+                return 0;
+
+            } else {
+                return 1;
+
+            }
+        } else if (A == B) {
+            if (A % K == 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
         int count = 0;
         int firstValue = 0;
-        for (int i = A; i <= A + K - 1; i++) {
-            if (i % K == 0) {
-                firstValue = i;
-            }
 
+        if (A + K - 1 <= B) {
+            for (int i = A; i <= A + K - 1; i++) {
+                if (i % K == 0) {
+                    firstValue = i;
+                    break;
+                }
+            }
+        } else {
+            for (int i = A; i <= B; i++) {
+                if (i % K == 0) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
         }
 
+        int distance = B - firstValue;
+        count = distance / K + 1;
 
-        for (int i = firstValue; i <= B; i += K) {
-            if (i % K == 0) {
-                count++;
-            }
-        }
 
         return count;
     }
