9973ac7947aed0cd4350b728f54ae2894f8d6c4a
Marcin Kowalczyk <kowalczyk.m@int.pl>
2021-05-05 18:09:07 +0200

    Zad 7_3 100%/100%

---
+++ src/main/java/pl/kowalczyk/codility/Zad_7_3.java
@@ -0,0 +1,60 @@
+package pl.kowalczyk.codility;
+
+import java.util.Stack;
+
+public class Zad_7_3 {
+    public static void main(String[] args) {
+        System.out.println(solution("{{{{"));
+        System.out.println(solution("{([][])}"));
+        System.out.println(solution("{[()()][()]}"));
+        System.out.println(solution("{[(12332112)(42343243)][(312321)]}"));
+
+    }
+
+    public static int solution(String S) {
+//        Deque<Character> stack = new ArrayDeque<Character>();
+        Stack<Character> stack = new Stack<Character>();
+        char lestChar;
+
+
+        if (S.isEmpty()) {
+            return 1;
+        }
+
+        for (int i = 0; i < S.length(); i++) {
+            char currentChar = S.charAt(i);
+            if (currentChar == '{' || currentChar == '[' || currentChar == '(') {
+                stack.push(currentChar);
+                continue;
+            }
+            if (stack.isEmpty()) {
+                return 0;
+            }
+
+            if (currentChar == '}') {
+                lestChar = stack.pop();
+                if (lestChar == '[' || lestChar == '('){
+                    return 0;
+                }
+            } else if (currentChar == ']') {
+                lestChar = stack.pop();
+                if (lestChar == '{' || lestChar == '('){
+                    return 0;
+                }
+            } else if (currentChar == ')') {
+                lestChar = stack.pop();
+                if (lestChar == '{' || lestChar == '[') {
+                    return 0;
+                }
+            }
+        }
+        if (!stack.isEmpty()){
+            return 0;
+        }
+
+
+        return 1;
+
+    }
+
+}
